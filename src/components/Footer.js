f8f814faca64fd6b87be9cb3f6ea9448dc94c611
Khai Hung <hungluongkhai@gmail.com>
2024-06-27 02:21:30 -0700

    Fix i18next missing issue

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
     return (
         <footer className="bg3 p-t-75 p-b-32">
             <div className="container">
-                <div className="row">                    
+                <div className="row">
                     <div className="col-sm-6 col-lg-3 p-b-50">
                         <Translation>{t =>  
                             <h5 className="stext-301 cl0 p-l-22  p-b-10">{t('workingTime1')}</h5>}
